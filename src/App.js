0395cd89e5b23470588e2b0a0dc59550e8f9691b
Isaac <isaacandrespozo@gmail.com>
2022-02-18 15:14:36 -0500

    React router dom added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Welcome from "./components/Welcome";
-const name = "Isaac";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Contador from "./pages/Contador";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Welcome message={name}/>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contacto</Link>
+              </li>
+              <li>
+                <Link to="/contador">Contador</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/contador" element={<Contador/>}/>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
---
+++ src/pages/Contador.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Welcome from '../components/Welcome';
+export default function Contador() {
+  return (
+    <div>
+        <Welcome message="Isaac"/>
+    </div>
+  )
+}
