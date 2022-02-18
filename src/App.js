1a846e5c9f28e0d74ce0e07a37ba8ecffa09ad09
Isaac <isaacandrespozo@gmail.com>
2022-02-18 14:07:55 -0500

    state changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Welcome from "./components/Welcome";
-const name = "Isaac Andres";
+const name = "Isaac";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Welcome message={name}/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,25 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 
-export default class Welcome extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <p>Hola {this.props.message} desde Welcome</p>
-      </div>
-    );
-  }
+export default function Welcome(props) {
+  const [counter, setCounter] = useState(0);
+  const [semaforo, setSemaforo] = useState(false);
+  useEffect(() => {
+    console.log(semaforo)
+  }, [semaforo])
+  
+  const contar = () => {
+    setCounter(counter + 1);
+    setSemaforo(!semaforo);
+  };
+  return (
+    <div>
+      <p>Hola {props.message}</p>
+      <p>Contador de React con Hooks</p>
+      <button type="submit" onClick={contar}>
+        Sumar Contador
+      </button>
+      <p>{counter}</p>
+      <p>El semáforo es de color: {semaforo ? "rojo" : "verde"}</p>
+    </div>
+  );
 }
