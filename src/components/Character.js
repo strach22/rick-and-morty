07aec467a79d242b4918cb541ca11bd7d9db0379
Isaac <isaacandrespozo@gmail.com>
2022-02-19 08:26:41 -0500

    Characters info

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,21 +1,48 @@
 export default function Character(props) {
   return (
     <div className="characters">
-        <h1>Personajes</h1>
+      <h1>Personajes</h1>
       <button onClick={console.log(props)} className="btn-search">
         Regresar
       </button>
       <div className="container-characters">
-        {props.characters.map(character=>(
+        {props.characters.map((character) => (
           <div className="character-container" key={character.id}>
-            <p>{character.name}</p>
-            <br/>
-            <img src={character.image}/>
+            <div>
+              <img src={character.image} alt={character.name} />
+            </div>
+            <div>
+              <h3>{character.name}</h3>
+              <h6>
+                {character.status == "Alive" ? (
+                  <>
+                    <span className="alive">Vivo</span>
+                  </>
+                ) : (
+                  <>
+                    <span className="dead">Muerto</span>
+                  </>
+                )}
+              </h6>
+              <p>
+                <span className="text-grey">Episodios:</span>
+                <span>{character.episode.length}</span>
+              </p>
+              <p>
+                <span className="text-grey">Gender:</span>
+                <span>{character.gender}</span>
+              </p>
+              <p>
+                <span className="text-grey">Specie:</span>
+                <span>{character.species}</span>
+              </p>
+            </div>
           </div>
-
         ))}
-
       </div>
+      <button onClick={console.log(props)} className="btn-search">
+        Regresar
+      </button>
     </div>
   );
 }
